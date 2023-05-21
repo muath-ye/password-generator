cfc225d2e7f4a6076fd56087e3a1937759fccb01
muathye <muath.ye@gmail.com>
2023-05-22 01:44:24 +0300

    feat: add complex options

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
 const crypto = require('crypto');
 const yargs = require('yargs');
 
-const generatePassword = (length = 12) => {
-  const characters = '[email protected]#$%^&*()_+-=';
+const generatePassword = ({ length = 12, symbols = false, numbers = false, uppercase = false, complex = false }) => {
+  let characters = 'abcdefghijklmnopqrstuvwxyz';
+  if (symbols || complex) {
+    characters += '!@#$%^&*()_+-=';
+  }
+  if (numbers || complex) {
+    characters += '0123456789';
+  }
+  if (uppercase || complex) {
+    characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
   const buffer = crypto.randomBytes(length);
   const password = Array.from(buffer)
     .map(byte => characters[byte % characters.length])
@@ -19,8 +28,38 @@ const argv = yargs
     type: 'number',
     default: 12
   })
+  .option('symbols', {
+    alias: 's',
+    description: 'Include symbols',
+    type: 'boolean',
+    default: false
+  })
+  .option('numbers', {
+    alias: 'n',
+    description: 'Include numbers',
+    type: 'boolean',
+    default: false
+ })
+  .option('uppercase', {
+    alias: 'u',
+    description: 'Include uppercase letters',
+    type: 'boolean',
+    default: false
+  })
+  .option('complex', {
+    alias: 'c',
+    description: 'Include numbers, symbols and uppercase letters',
+    type: 'boolean',
+    default: false
+  })
   .help()
   .alias('help', 'h')
   .argv;
 
-console.log(generatePassword(argv.length));
+console.log(generatePassword({
+  length: argv.length,
+  symbols: argv.symbols,
+  numbers: argv.numbers,
+  uppercase: argv.uppercase,
+  complex: argv.complex
+}));
